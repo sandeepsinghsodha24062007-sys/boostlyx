417561b475d95b542c44ee87d2b99eec8fe0374f
sandeepsinghsodha24062007-sys <sandeepsinghsodha24062007@gmail.com>
2026-04-03 17:18:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function pay() {
-window.location.href = "upi://pay?pa=sbehhejebns@ibl&pn=SANDEEP%20SINGH";
-setTimeout(() => {
-document.getElementById("process").style.display = "block";
-}, 2000);
+function pay(){
+window.location.href="upi://pay?pa=sbehhejebns@ibl&pn=SANDEEP%20SINGH";
+
+setTimeout(()=>{
+document.getElementById("loading").style.display="block";
+
+let time=5;
+let timer=setInterval(()=>{
+time--;
+document.getElementById("timer").innerText=time;
+
+if(time==0){
+clearInterval(timer);
+document.getElementById("loading").style.display="none";
+document.getElementById("success").style.display="block";
+}
+},1000);
+
+},2000);
 }
