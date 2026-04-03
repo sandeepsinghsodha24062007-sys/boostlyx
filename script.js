bc3046acbc679190e54530c2b6521dff720e3c73
sandeepsinghsodha24062007-sys <sandeepsinghsodha24062007@gmail.com>
2026-04-03 18:14:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 function pay(){
+
 window.location.href="upi://pay?pa=sbehhejebns@ibl&pn=SANDEEP%20SINGH";
 
 setTimeout(()=>{
 document.getElementById("loading").style.display="block";
 
-let time=5;
-let timer=setInterval(()=>{
-time--;
-document.getElementById("timer").innerText=time;
-
-if(time==0){
-clearInterval(timer);
+setTimeout(()=>{
 document.getElementById("loading").style.display="none";
-document.getElementById("success").style.display="block";
-}
-},1000);
+document.getElementById("done").style.display="block";
+},4000);
 
 },2000);
+
 }
